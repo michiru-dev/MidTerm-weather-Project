a01679a14b76c4b605ac58fc60795e597a37dbd2
michiru6828 <since_l999@yahoo.co.jp>
2023-04-11 15:40:00 -0700

    3hour range section done

--- JS/weatherAPIs.js
+++ JS/weatherAPIs.js
@@ -27,21 +27,32 @@ const fetchWeather = async (lat, lon) => {
 const fetch5daysWeather = async (lat, lon) => {
     const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=b096135c0ecd098d7bef5ed1f3046a48`)
     const jsonData2 = await res.json()
+    console.log(jsonData2)
+
+    const currentDate = new Date();
+    const futureDate = new Date();
+
+
+    futureDate.setDate(currentDate.getDate() + 1);
+    futureDate.setHours(0, 0, 0);
 
     const dailyData = jsonData2.list.reduce((acc, data) => {
         const dateUTC = new Date(Date.parse(data.dt_txt));
-        const date = dateUTC.toLocaleString('en-US', { weekday: 'short', month: 'long', day: 'numeric', timeZone: 'America/Vancouver' });
-        if (!acc[date]) {
-            acc[date] = [];
+        if (dateUTC.getDate() >= futureDate.getDate()) {
+            const date = dateUTC.toLocaleString('en-US', { weekday: 'short', month: 'long', day: 'numeric', timeZone: 'America/Vancouver' });
+            if (!acc[date]) {
+                acc[date] = [];
+            }
+            acc[date].push(data);
         }
-        acc[date].push(data);
         return acc;
     }, {});
 
+    console.log(dailyData)
 
     // Calculate the average temperature for each day
     const dailyTemperatureData = Object.entries(dailyData).map(([date, data]) => {
-        const maxTemp = Math.max(...data.map(item => item.main.temp_max)) - 273.15;
+        const maxTemp = Math.max(...data.map(item => item.main.temp_max)) - 273.15;//...でカッコ外す
         const minTemp = Math.min(...data.map(item => item.main.temp_min)) - 273.15;
         const avgTemp = data.reduce((acc, item) => acc + item.main.temp, 0) / data.length - 273.15;
         const description = data[0].weather[0].description;
@@ -57,6 +68,7 @@ const fetch5daysWeather = async (lat, lon) => {
         const weatherElement = document.getElementById(`weather${dayIndex}`);
         const tempElement = document.getElementById(`temp${dayIndex}`);
         const iconElement = document.getElementById(`icon${dayIndex}`);
+
         if (dateElement && weatherElement && tempElement && iconElement) {
             dateElement.textContent = data.date;
             weatherElement.textContent = data.weather;
@@ -65,7 +77,39 @@ const fetch5daysWeather = async (lat, lon) => {
             iconElement.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weatherIcon}.png">`;
         }
     });
+
+
+    //get 3hours
+    const getDate = document.querySelectorAll(".date-link")
+
+    //getDateに直接アドイベントできない理由はgetDateがリストだから
+    //Allで取ってきてるから
+    getDate.forEach(function (dateDiv) {
+        dateDiv.addEventListener("click", function (e) {
+            const targetId = dateDiv.children[0].children[0].id
+            const dateSpan = this.querySelector(`#${targetId}`)
+            const dateText = dateSpan.textContent
+            //dailyDataのオブジェクトにキー（日にち）でアクセス
+            dailyData[dateText].forEach((hourGap, index) => {
+                const getHourGap = document.getElementById(`hourGap${index}`)
+                const newSpan = document.createElement("span")
+                newSpan.textContent = `${(hourGap.main.temp - 273.15).toFixed(1)} °C`
+                const newP = document.createElement("p")
+                newP.textContent = hourGap.weather[0].description
+                const newDiv = document.createElement("div")
+                newDiv.innerHTML = `<img src="http://openweathermap.org/img/wn/${hourGap.weather[0].icon}.png">`
+                getHourGap.replaceChildren()
+                getHourGap.appendChild(newSpan)
+                getHourGap.appendChild(newP)
+                getHourGap.appendChild(newDiv)
+            }
+
+            )
+
+        })
+    })
 }
+
 
 //Current Weather
 const getWeather = () => {
@@ -96,7 +140,4 @@ const getWeather = () => {
 }
 
 getWeather()
-
-
-// //get 3hours
 
